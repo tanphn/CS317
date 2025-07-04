36e26cae612be60e1a65f161d1204d6c694293dd
nguoimay1103 <136198806+nguoimay1103@users.noreply.github.com>
2025-07-04 15:27:49 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Request
 from prometheus_fastapi_instrumentator import Instrumentator
 from predictor import predict_image
 from PIL import Image
 import io
 import logging
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from prometheus_client import Summary, Gauge
 
 app = FastAPI()
 
 # Cấu hình logging
 logging.basicConfig(filename='/app/app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Gắn kết thư mục tĩnh (CSS, JavaScript)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Thiết lập Jinja2 templates
+templates = Jinja2Templates(directory="templates")
 
 # Định nghĩa metric tùy chỉnh
 inference_time_summary = Summary('model_inference_time_seconds', 'Time spent in model inference', ['job'])
@@ -19,6 +27,11 @@ confidence_score = Gauge('model_confidence_score', 'Model confidence score')
 # Thêm Prometheus instrumentation
 instrumentator = Instrumentator()
 instrumentator.instrument(app).expose(app)
+
+# Endpoint để phục vụ giao diện web
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
